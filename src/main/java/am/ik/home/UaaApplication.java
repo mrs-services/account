c108fafc5483ef53c7e9d6120352ae7e8ad50b29
making <makingx@gmail.com>
2016-09-26 23:38:08 +0900

    Update for MRS

--- src/main/java/am/ik/home/UaaApplication.java
+++ src/main/java/am/ik/home/UaaApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -46,6 +47,7 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UaaApplication {
 
     public static void main(String[] args) {
